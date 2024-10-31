84d7ac3443abf548d2a13920d46ec94a7ec977d6
Ayman <aymanahmedpersonal@gmail.com>
2024-10-31 19:15:59 +0600

    Initial Commit

--- server.js
+++ server.js
@@ -5,8 +5,6 @@ const multer = require('multer');
 const path = require('path');
 
 const app = express();
-const port = 3000;
-
 let pythonMessages = [];
 let webpageMessages = [];
 
@@ -70,6 +68,7 @@ app.get('/', (req, res) => {
     res.sendFile(__dirname + '/public/index.html');
 });
 
-app.listen(port, () => {
+const PORT = process.env.PORT || 3000;
+app.listen(PORT, () => {
     console.log(`Node.js server running at http://localhost:${port}`);
 });
