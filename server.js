7e1eeb14ed16eb903f1ad301aab270e3079e6737
Ayman-0-Ahmed <aymanahmedpersonal@gmail.com>
2025-11-19 10:40:56 +0600

    final

--- server.js
+++ server.js
@@ -1,13 +1,26 @@
-const WebSocket = require('ws');
+const express = require('express')
+const { createServer } = require('http')
+const WebSocket = require('ws')
 
-const targetUrl = process.env.TARGET_URL || 'ws://localhost:8080';
-const ws = new WebSocket(targetUrl);
+const app = express()
+const server = createServer(app)
+const port = process.env.PORT || 10000
 
-ws.on('open', () => {
-  console.log('Connected to listener');
-  ws.send('run demo_task');
-});
+const wss = new WebSocket.Server({ server, path: '/ws' })
 
-ws.on('message', msg => {
-  console.log('Received reply:', msg.toString());
-});
+app.get('/', (req, res) => {
+  res.send('Hello over HTTP!')
+})
+
+wss.on('connection', (ws) => {
+  console.log('WebSocket client connected')
+
+  ws.on('message', (message) => {
+    console.log('Received:', message.toString())
+    ws.send(`Hello over WebSocket!`)
+  })
+})
+
+server.listen(port, () => {
+  console.log(`Server listening on port ${port}`)
+})
